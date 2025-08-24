6f3ed5a7666099ff9fab0c528636366a4ad5d0d7
Jens-Aime <jensng7@gmail.com>
2025-08-25 01:01:13 +0200

    Initial commit – Job Swipe Projektstart

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+﻿import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const JobBlock());
+}
+class JobBlock extends StatelessWidget {
+  const JobBlock({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'JOB SWIPE',
+      home: const HomePage(),
+
+    );
+  }
+
+}
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                  "JOB",
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    fontFamily:'Roboto',
+                    height: 1.0,
+                    letterSpacing: 3.0,
+                  )),
+              const Text(
+                  "swipe",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w300,
+                    fontFamily:'Roboto',
+                    height: 0.50,
+                  )),
+            ],
+          ),
+        ),
+      );
+  }
+}
---
+++ lib/widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                  "JOB",
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    fontFamily:'Roboto',
+                    height: 1.0,
+                    letterSpacing: 3.0,
+                  )),
+              const Text(
+                  "swipe",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w300,
+                    fontFamily:'Roboto',
+                    height: 0.50,
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
